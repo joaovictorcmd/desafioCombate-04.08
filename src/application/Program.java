e8ae3c9bb12950da94fff81fa9ace5a791a35769
João Victor Carvalho <joaocarvalho.cmd@gmail.com>
2023-02-26 18:50:00 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.TaxPayer;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos contribuintes você vai digitar? ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        List<TaxPayer> list = new ArrayList<>();
+
+        for (int i=0;i<n;i++) {
+            System.out.printf("Digite os dados do %do contribuinte:\n", i + 1);
+            System.out.print("Renda anual com salário: ");
+            double salaryIncome = sc.nextDouble();
+            System.out.print("Renda anual com prestação de serviço: ");
+            double servicesIncome = sc.nextDouble();
+            System.out.print("Renda anual com ganho de capital: ");
+            double capitalIncome = sc.nextDouble();
+            System.out.print("Gastos médicos: ");
+            double healthSpending = sc.nextDouble();
+            System.out.print("Gastos educacionais: ");
+            double educationSpending = sc.nextDouble();
+
+            TaxPayer tPayer = new TaxPayer(salaryIncome, servicesIncome, capitalIncome, healthSpending, educationSpending);
+
+            list.add(tPayer);
+        }
+
+        System.out.println();
+
+        for (int i=0;i<n;i++) {
+            System.out.printf("Resumo do %do contribuinte:\n", i + 1);
+            System.out.println(list.get(i));
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
